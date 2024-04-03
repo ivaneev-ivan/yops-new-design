f30239c7daef12b8f8aa804419d3ebd369f076ee
ivan <ivaneev.ivane@gmail.com>
2024-04-03 18:35:24 +0300

    next config fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Home from '@/components/screens/Home';
-import { Service } from '@/components/types';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_ROOT as string;
 
 export const metadata = {
   title: 'Главная страница',
   description: '',
 };
 
-const getServices: () => Promise<Service[]> = async () => {
-  const response = await fetch(`${BASE_URL}/api/locations/`, { next: { revalidate: 3600 }, mode:'no-cors' });
-  return (await response.json()) as Service[];
-};
-
-export default async function HomePage() {
-  const services = await getServices();
-  return <Home services={services} />;
+export default function HomePage() {
+  return <Home services={[]}/>;
 }
